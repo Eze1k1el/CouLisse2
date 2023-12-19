5195d9b8a55ffca5692846f74c38416ce0e39ece
clementagathe <agathe.clement@edu.univ-fcomte.fr>
2023-12-19 14:43:38 +0100

    Aled l'UI

---
+++ Scor.gd
@@ -0,0 +1,12 @@
+extends Control
+var score= 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+func add_score(add:int):
+	score+=add
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
