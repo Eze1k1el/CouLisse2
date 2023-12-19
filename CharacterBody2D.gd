ade6d279b5fa72ca3e4d4a40c36abff8df7e83a0
Eze1k1el <154252496+Eze1k1el@users.noreply.github.com>
2023-12-19 14:28:07 +0100

    Update CharacterBody2D.gd

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -20,11 +20,18 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var directionslide = Input.get_axis("ui_left", "ui_right")
+	if directionslide:
+		velocity.x = directionslide * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	var directionupdown = Input.get_axis("ui_up", "ui_down")
+	if directionupdown:
+		velocity.y = directionupdown * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
 
 	move_and_slide()
 
