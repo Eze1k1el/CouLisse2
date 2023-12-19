e44cf8808bc7d726232c3a11791ffaf2f95f40c7
FrancisRave <francois.janot@laposte.net>
2023-12-19 11:46:59 +0100

    Avion qui avance
    scène avion

--- AvionBouge.gd
+++ AvionBouge.gd
@@ -1,13 +1,31 @@
 extends StaticBody2D
 
 const SPEED = 100
+var hmin = 750
+var hmax = 250
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+#
+#func _get_h():
+	#var h
+	#if position.y <= 750 :
+		#h = true
+	#elif position.y >= 250:
+		#h = false
+	#return h
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.x += 10
-	
+	#fonction qui renvoie true si hmax et false si hmin
+	#if _get_h():
+		#position.y += 10
+	#else :
+		#position.y -= 10
+		#
+func _physics_process(delta):
+	if get_viewport().get_visible_rect().size.x + 100 <position.x :
+		position.x = -1000
 
