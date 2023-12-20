c67570ae85ac32f3c53e27ce088af5f82fe7903e
FrancisRave <francois.janot@laposte.net>
2023-12-20 11:25:47 +0100

    Giraffe flipflip + sprite girafe

--- giraffe_movement_auto.gd
+++ giraffe_movement_auto.gd
@@ -1,7 +1,8 @@
 extends StaticBody2D
 var variable_mult = -1
 var TimerStart = true
-var hauteurgiraffe = randf_range(500, 1000)
+var FlipFlip = true
+var hauteurgiraffe = randf_range(800, 1200)
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
@@ -11,10 +12,15 @@ func _process(delta):
 	position.y += variable_mult*10
 	if position.y <= hauteurgiraffe:
 		_tgrenaud()
+		_flipflip()
+		
 
-
+func _on_timerflip_timeout():
+	scale.x = -1
+	
 func _on_timer_timeout():
 	variable_mult = 1
+	scale.x = 1
 	print("on redescend")
 	
 	pass # Replace with function body.
@@ -22,9 +28,16 @@ func _on_timer_timeout():
 func _tgrenaud():
 	while TimerStart == true :
 		variable_mult = 0
-		$Timer.start()
+		$Timerdescends.start()
 		print("on sarrete")
 		TimerStart = false
 		
+func _flipflip():
+	while FlipFlip == true :
+		$Timerflip.start()
+		FlipFlip = false
+		
 
-	
+
+
+
