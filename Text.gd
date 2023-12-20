9a6f29ecff718f1fea793e652c4dc6accf4ee1e3
clementagathe <agathe.clement@edu.univ-fcomte.fr>
2023-12-20 14:36:19 +0100

    Intro complete

--- Text.gd
+++ Text.gd
@@ -1,9 +1,1 @@
-extends Node2D
 
-
-const lines: Array[String] = [
-	"Yo c'est Godot",
-	"J'ai réalisé un jeu stylé!",
-	"C'est sur le fait d'attendre Godot 4, on l'a attendu depuis tellement longtemps",
-	"blabla",
-]
