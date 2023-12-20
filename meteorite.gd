2a0d48030d5b311a91a20afb790cea3122a380d1
maxime 2 <berland.sophie@free.fr>
2023-12-20 17:26:54 +0100

    satelite et meteorite

--- meteorite.gd
+++ meteorite.gd
@@ -4,6 +4,8 @@ var depx = randf_range(-7, 7)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if depx < -2 :
+		rotation += 1.1
 	position.y = -350
 	position.x = randf_range(660,1260,)
 	$AudioStreamPlayer2D.play()
